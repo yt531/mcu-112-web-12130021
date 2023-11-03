6e783a9c3f7025cdf128da20f6a1ca382ccd2e34
yt531 <yanting0531@gmail.com>
2023-11-03 16:34:17 +0800

    feat(ts): 利用數值變數記錄待辦事項編號

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-let todoB = "Todo B";
-console.log(todoB);
+let todoId = number;
+todoId = 1;
+console.log("Todo ID", todoId);
 
-const todoC = "Todo C";
-console.log(todoC);
+const todo = "期中考作業";
+console.log("Todo Name:", todo);
