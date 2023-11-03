ba1d20d2d9d9056bbdbefa5ef910b7f1f703dcf3
yt531 <yanting0531@gmail.com>
2023-11-03 17:07:17 +0800

    feat(ts): 利用類別方法設定待辦事項已完成

--- src/todo.ts
+++ src/todo.ts
@@ -2,8 +2,13 @@ export class Todo {
   constructor(
     public id: number,
     public content: string,
-    public hasFinished = false
+    public hasFinished: boolean = false
   ) {}
 
   finishDate: Date;
+
+  setFinished(finishDate: Date): void {
+    this.hasFinished = true;
+    this.finishDate = finishDate;
+  }
 }
