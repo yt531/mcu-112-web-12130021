bd82caffeb1c9ea49b69df4385f26e0e1cf3524e
yt531 <yanting0531@gmail.com>
2023-11-17 16:23:25 +0800

    feat(todo): 利用文字插值顥示待辦事項內容

--- src/assets/code/index.ts
+++ src/assets/code/index.ts
@@ -1,9 +1,9 @@
-import { Todo } from "./todo";
+import { Todo } from ''./todo';
 
 const todos = [
-  new Todo(1, "期中考作業"),
-  new Todo(2, "期中考作業-2"),
-  new Todo(3, "期中考作業-3"),
+  new Todo(1, '期中考作業'),
+  new Todo(2, '期中考作業-2'),
+  new Todo(3, '期中考作業-3'),
 ];
 
 todos[0].setFinished(new Date());
