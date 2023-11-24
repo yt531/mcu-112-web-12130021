dae79f1a1961982ed07ade965d5f9c2f4897a5e6
yt531 <yanting0531@gmail.com>
2023-11-24 15:43:06 +0800

    feat(header): 實作待辦事項頁首元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TodoComponent],
+  imports: [HeaderComponent, TodoComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
