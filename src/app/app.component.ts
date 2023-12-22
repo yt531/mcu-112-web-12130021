d41b86eac317198b81aa91f07a21fd204fd7fc55
yt531 <yanting0531@gmail.com>
2023-12-22 15:36:00 +0800

    feat(todo-search): 建立待辦事項查詢表單元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Todo } from './model/todo';
 import { TaskService } from './services/task.service';
 import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoSearchComponent } from './todo-search/todo-search.component';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +19,7 @@ import { TodoListComponent } from './todo-list/todo-list.component';
     HeaderComponent,
     TodoListComponent,
     TodoDetailComponent,
+    TodoSearchComponent,
     FooterComponent,
   ],
   templateUrl: './app.component.html',
