0de055c9eb27965e5764eec5fd5668d042cf91d9
yt531 <yanting0531@gmail.com>
2023-12-29 21:35:21 +0800

    feat(todo-form): 待辦事項頁面標題顥示實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,24 @@
 import { Routes } from '@angular/router';
-
 import { LoginPageComponent } from './login-page/login-page.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { TodoDetailPageComponent } from './todo-detail-page/todo-detail-page.component';
 import { TodoFormPageComponent } from './todo-form-page/todo-form-page.component';
 import { TodoPageComponent } from './todo-page/todo-page.component';
-
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: TodoPageComponent },
   { path: 'todo/:id', component: TodoDetailPageComponent },
-  { path: 'todo-form', component: TodoFormPageComponent },
-  { path: 'todo-form/:id', component: TodoFormPageComponent },
+  {
+    path: 'todo-form',
+    component: TodoFormPageComponent,
+    data: { title: '待辦事項新增' },
+  },
+  {
+    path: 'todo-form/:id',
+    component: TodoFormPageComponent,
+    data: { title: '待辦事項編輯' },
+  },
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
   { path: '**', component: NotFoundPageComponent },
--- src/app/todo-form-page/todo-form-page.component.spec.ts
+++ src/app/todo-form-page/todo-form-page.component.spec.ts
@@ -1,23 +1,51 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map, Observable, switchMap, tap } from 'rxjs';
+import { Todo } from '../model/todo';
+import { TaskService } from '../services/task.service';
+import { TodoFormComponent } from '../todo-form/todo-form.component';
+@Component({
+  selector: 'app-todo-form-page',
+  standalone: true,
+  imports: [TodoFormComponent],
+  templateUrl: './todo-form-page.component.html',
+  styleUrl: './todo-form-page.component.css',
+})
+export class TodoFormPageComponent implements OnInit {
+  taskService = inject(TaskService);
 
-import { TodoFormPageComponent } from './todo-form-page.component';
+  title!: string;
 
-describe('TodoFormPageComponent', () => {
-  let component: TodoFormPageComponent;
-  let fixture: ComponentFixture<TodoFormPageComponent>;
+  id?: number;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TodoFormPageComponent]
-    })
-    .compileComponents();
+  formData?: Todo;
+  readonly router = inject(Router);
+  readonly route = inject(ActivatedRoute);
+  ngOnInit(): void {
+    this.route.paramMap
+      .pipe(
+        filter((paramMap) => paramMap.has('id')),
+        map((paramMap) => +paramMap.get('id')!),
+        tap((id) => (this.id = id)),
+        switchMap((id) => this.taskService.getById(id))
+      )
+      .subscribe((formData) => (this.formData = formData));
 
-    fixture = TestBed.createComponent(TodoFormPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    this.route.data
+      .pipe(map(({ title }) => title))
+      .subscribe((title) => (this.title = title));
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  onSave(task: Todo): void {
+    let action$: Observable<Todo>;
+    if (this.id) {
+      action$ = this.taskService.update(this.id, task);
+    } else {
+      action$ = this.taskService.add(task);
+    }
+    action$.subscribe(() => this.onCancel());
+  }
+  onCancel(): void {
+    this.router.navigate(['home']);
+  }
+}
