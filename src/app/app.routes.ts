9d81eb8b00a92a618cf9de362460310ff593ba54
yt531 <yanting0531@gmail.com>
2023-12-29 19:50:56 +0800

    feat(home): 待辦事項頁面路由組態設定

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { TodoPageComponent } from './todo-page/todo-page.component';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: TodoPageComponent },
+];
