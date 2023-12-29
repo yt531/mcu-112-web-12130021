399df3cdce143c02cae6a9ea43a93634ce30efa1
yt531 <yanting0531@gmail.com>
2023-12-29 21:39:28 +0800

    feat(todo-form): 待辦事項預載資料實作

--- src/app/todo-form-page/todo-form-page.component.ts
+++ src/app/todo-form-page/todo-form-page.component.ts
@@ -1,11 +1,9 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { filter, map, Observable, switchMap, tap } from 'rxjs';
-
 import { Todo } from '../model/todo';
 import { TaskService } from '../services/task.service';
 import { TodoFormComponent } from '../todo-form/todo-form.component';
-
 @Component({
   selector: 'app-todo-form-page',
   standalone: true,
@@ -15,6 +13,7 @@ import { TodoFormComponent } from '../todo-form/todo-form.component';
 })
 export class TodoFormPageComponent implements OnInit {
   taskService = inject(TaskService);
+  title!: string;
   id?: number;
   formData?: Todo;
   readonly router = inject(Router);
@@ -23,12 +22,18 @@ export class TodoFormPageComponent implements OnInit {
     this.route.paramMap
       .pipe(
         filter((paramMap) => paramMap.has('id')),
-        map((paramMap) => +paramMap.get('id')!),
-        tap((id) => (this.id = id)),
-        switchMap((id) => this.taskService.getById(id))
+        map((paramMap) => +paramMap.get('id')!)
+      )
+      .subscribe((id) => (this.id = id));
+
+    this.route.data
+      .pipe(
+        tap(({ title }) => (this.title = title)),
+        map(({ formData }) => formData)
       )
       .subscribe((formData) => (this.formData = formData));
   }
+
   onSave(task: Todo): void {
     let action$: Observable<Todo>;
     if (this.id) {
